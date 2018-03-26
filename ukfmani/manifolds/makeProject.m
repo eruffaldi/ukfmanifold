a82122f750c86a6b7e9d3bcf17a2bf1fce5305f8
Emanuele Ruffaldi <emanuele.ruffaldi@gmail.com>
2018-03-26 15:07:04 +0200

    makeProject comments

--- ukfmani/manifolds/makeProject.m
+++ ukfmani/manifolds/makeProject.m
@@ -9,12 +9,25 @@
 %
 % Note that the state is still in the original frame
 %
+% m = makeProject(m0,R)
+%   applis the linear transformation R
+%
+% m = makeProject(m0,R,k)
+%   applies the transformation R and takes the first k tangent vectors
+%
+% The transformation can be buil using:
+%   adjoint matrix (6x6) for SE(3) to constraint along direction
+%   rotation matrix (3x3) for SO(3)
+%   permutatio of axis (permVector)
 % 
 %
 % Emanuele Ruffaldi 2017-2018 @ SSSA
 function m = makeProject(m0,R,k)
 
 n=m0.alg;
+if nargin == 2
+    k=n;
+end
 assert(k <= n && k > 0,'new space should have correct number of dofs');
 assert(all(size(R)==n),'R should be n by n');
 %assert R not singular
@@ -24,12 +37,20 @@ m.refmodels = {m0};
 m.type = {'Project',m0.type,R};
 m.alginc = [0,k];
 ostep=m0.step;
-m.step = @(X,v) ostep(X,(R\zeroextendcol(v,n,k))');
-m.delta =@(X,Y) firstk(R*m0.delta(X,Y)',k)';
-m.transport = @(X,t,Y) xtransport(X,t,Y,m0.transport,n,k);
+
 m.islie = m0.islie;
 m.unpack = @(x) projectunpack(m0,x);
 m.pack = @(x) m0.pack(x);
+
+
+if n == k
+    m.step = @(X,v) ostep(X,(R\v(:))');
+    m.delta =@(X,Y) (R*m0.delta(X,Y)')';
+else
+    m.step = @(X,v) ostep(X,(R\zeroextendcol(v,n,k))');
+    m.delta =@(X,Y) firstk(R*m0.delta(X,Y)',k)';
+end
+m.transport = @(X,t,Y) xtransport(X,t,Y,m0.transport,n,k);
 m.projectcov = @(C) projectcov(C,R,k);
 m.unprojectcov = @(C) unprojectcov(C,R,k,n);
 m.s = int_manisetup([],[],m);
@@ -39,8 +60,13 @@ m.s = int_manisetup([],[],m);
 if isfield(m0,'log')
     olog=m0.log;
     oexp=m0.exp;
-    m.log = @(X) firstk((R*reshape(olog(X),[],1)),k)';
-    m.exp = @(v) oexp((R\zeroextendrow(v,n,k))');
+    if n == k
+        m.log = @(X) (R*olog(X))';
+        m.exp = @(v) oexp((R\v(:))');
+    else
+        m.log = @(X) firstk((R*reshape(olog(X),[],1)),k)';
+        m.exp = @(v) oexp((R\zeroextendrow(v,n,k))');
+    end
 end
 
 function v = xtransport(X,t,Y,tra,n,k)
